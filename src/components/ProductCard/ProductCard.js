47f99678abdddd39e18b529df9bd7ff03940b1bb
Aditya Wiraha <squalvj@gmail.com>
2020-02-08 18:38:19 +0700

    add product card component and integrate with frontend

---
+++ src/components/ProductCard/ProductCard.js
@@ -0,0 +1,28 @@
+import React from 'react'
+import './ProductCard.scss';
+export default function ProductCard({productName, productPrice, productDesc, productStyles, productDelivery}) {
+  return (
+    <div className="wrapper-product-card padding-15">
+      <div className="flex j-space-between">
+        <div className="wrapper-product-name">
+          <h1>{productName}</h1>
+        </div>
+        <div className="wrapper-product-price">
+          <h2>{productPrice}</h2>
+        </div>
+      </div>
+
+      <div className="wrapper-product-description flex f-column">
+        <p>{productDesc}</p>
+        <span className="product-styles">
+          {
+            (productStyles || []).join(', ')
+          }
+        </span>
+        <span className="product-delivery">
+          {productDelivery}
+        </span>
+      </div>
+    </div>
+  )
+}
