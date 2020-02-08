623d8325ecf027497f211a9fc9ae93339378e983
Aditya Wiraha <squalvj@gmail.com>
2020-02-08 22:11:12 +0700

    Updates

---
+++ precache-manifest.6099fe3139ff5617c0ee68364d8a6f5b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "722c4460edb84b10c9046169539be473",
+    "url": "/fabelio/index.html"
+  },
+  {
+    "revision": "4fc6b6a1999f265e3211",
+    "url": "/fabelio/static/css/main.6cf31926.chunk.css"
+  },
+  {
+    "revision": "f1563af0ed538cd9d9b5",
+    "url": "/fabelio/static/js/2.87c0754a.chunk.js"
+  },
+  {
+    "revision": "4fc6b6a1999f265e3211",
+    "url": "/fabelio/static/js/main.fd388570.chunk.js"
+  },
+  {
+    "revision": "41cb5fdda380da521982",
+    "url": "/fabelio/static/js/runtime~main.0524fdfa.js"
+  },
+  {
+    "revision": "b1337439dec428093ace1682bbcba96a",
+    "url": "/fabelio/static/media/nyan.b1337439.gif"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.5028cb0e1b2248a0b2702b3104bc2154.js"
+  "/fabelio/precache-manifest.6099fe3139ff5617c0ee68364d8a6f5b.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/fabelio/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
 });
